1418539b48bc39e5be5b6203239e4ed9d26f0982
lucianalml <limao.luciana@gmail.com>
2016-06-06 18:31:34 -0300

    Configurando dashboards

--- resources/views/admin/layouts/base.blade.php
+++ resources/views/admin/layouts/base.blade.php
@@ -8,8 +8,19 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 	<script src="https://use.fontawesome.com/4d61063bad.js"></script>
+	
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="{{ URL::to('/css/admin/admin.css') }}">
 
-	<link rel="stylesheet" href="{{ URL::to('/css/admin.css') }}">
+	<!-- Timeline CSS -->
+    <link href="{{ URL::to('/css/admin/timeline.css') }}" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+	<link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.5.1.css">
+
+    <!-- MetisMenu CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/metisMenu/2.5.2/metisMenu.min.css" rel="stylesheet">
+
 
 	@yield('styles')
 </head>
@@ -23,8 +34,16 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
+<!-- Metis Menu Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/metisMenu/2.5.2/metisMenu.min.js"></script>
+
+<!-- Morris Charts JavaScript -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="http://cdn.oesmith.co.uk/morris-0.5.1.min.js"></script>
+<script src="{{ URL::to('/js/admin/morris-data.js') }}"></script>
+
 <!-- Custom Theme JavaScript -->
-<script type="text/javascript" src="{{ asset('js/admin/admin.js') }}"></script>
+<script type="text/javascript" src="{{ URL::to('/js/admin/admin.js') }}"></script>
 
 @yield('scripts')
 
